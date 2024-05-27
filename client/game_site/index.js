8e43cc0a0e29e0f1c459005bd439eaf0dc621182
Syed Saad Hussain <syhussai@adobe.com>
2024-05-27 14:30:30 +0530

    use node for client serving

--- client/game_site/index.js
+++ client/game_site/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(function(req, res, next) {
     res.setHeader("Cross-Origin-Opener-Policy", "same-origin"),
@@ -14,4 +14,4 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server listening on port: ${PORT}`));
